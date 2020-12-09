18d312702e4430a756a305e09025d122147d685b
卢方舟 <735417031@qq.com>
2020-12-09 15:17:40 +0800

    project init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
+//路由
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+/*ReactDOM.render(
+ /!* <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>,*!/
+  <App />,
   document.getElementById('root')
-);
+);*/
+/**********************study***********************/
+const tabList =
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+const user = {
+  firstName: 'personA',
+  lastName: 'personB'
+}
+const h1Style = {
+  fontSize: 100,
+  color: 'blue'
+}
+const arr = [3,4,5]
+function formatUser(user) {
+  console.log(26, user)
+  return user.firstName
+}
+ReactDOM.render(
+  <div>
+    <h1 style={h1Style}>lfz</h1>
+    {tabList}
+    <h2>{formatUser(user)}</h2>
+    {arr}
+  </div>,
+  document.getElementById('test')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
